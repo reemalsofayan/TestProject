23e3d9a4ede31db1bf8601b504329e326d139e51
reem kalid <reemalsofayan@gmail.com>
2021-07-23 14:34:12 +0300

    d

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,15 +1,10 @@
 
 public class Test {
-	public static void main(String[]arg){
-	
+	public static void main(String[] arg) {
 
-	
+		for (int i = 0; i < 5; i++)
+			System.out.println("hello world saradgd");
 
-		System.out.println("hello world saradgd");
-		System.out.println("hello world amerah");
-		System.out.println("hello world sargwrgwgwd");
-		System.out.println("hello world saradgd");
-	
 	}
 
 }
