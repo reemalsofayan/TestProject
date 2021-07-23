2cdcf0e8db67ac09ca542d1fede9b69f718faa79
reem kalid <reemalsofayan@gmail.com>
2021-07-23 13:16:13 +0300

    fifth commit

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,8 +2,7 @@
 public class Test {
 	public static void main(String[]arg){
 	
-		System.out.println("hello world reem");
-		System.out.println("hello world");
+		System.out.println("hello world everyone");
 	
 	
 	
