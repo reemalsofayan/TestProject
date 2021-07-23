f8c485801902bc0c286b171f399a9807223783be
reem kalid <reemalsofayan@gmail.com>
2021-07-23 14:41:36 +0300

    l

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,10 +2,7 @@
 public class Test {
 	public static void main(String[] arg) {
 
-		for (int i = 0; i < 5; i++)
-			System.out.println("hello world saradgd");
-		System.out.println("al obrash");
+		System.out.println("aefwegewgf");
 
 	}
-
 }
